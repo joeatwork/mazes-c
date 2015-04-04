b833fd4cad12839d495b7c5911af78ad167f4536
Joe Bowers <joerbowers@gmail.com>
2015-04-04 19:48:53 +0000

    Annotate dot output with better labels and top secret _x,_y
    This to facilitate being read by other maze-aware executables

--- dotter.c
+++ dotter.c
@@ -4,6 +4,8 @@ void mazes_dot(struct mazes_maze *maze, unsigned int *colors, FILE *stream) {
   fprintf(stream, "/** MAZE: Rectangle %d %d **/\n", maze->column_count, maze->row_count);
   fprintf(stream, "digraph Maze {\n");
   for (struct mazes_cell *cell = mazes_first_cell(maze); NULL != cell; cell = cell->next) {
+    fprintf(stream, "    node_%d_%d [shape=box,label=\"%d,%d\",_x=%d,_y=%d];\n",
+	    cell->column, cell->row, cell->column, cell->row, cell->column, cell->row);
     for (size_t i = 0; i < cell->links_length; i++) {
       struct mazes_cell *other = cell->links[i];
       fprintf(stream, "    node_%d_%d -> node_%d_%d;\n",
