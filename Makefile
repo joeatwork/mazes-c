9af3226a60ee0ce822a36729768678df41022b59
Joe Bowers <joe.bowers@coreos.com>
2015-11-11 13:28:41 -0800

    Clean up build for ubuntu

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
 OBJECTS=algorithms.o distances.o dotter.o main.o mazes.o pnger.o printer.o utils.o
 CFLAGS= -g -Wall -Werror -O3 -std=c11
 LDFLAGS= -lcairo
-CC=gcc-4.7
+CC=gcc
 
 all: mazes
 
 mazes: $(OBJECTS)
+	$(CC) $(CFLAGS) $(OBJECTS) -o $@ $(LDFLAGS)
 
 clean:
 	rm $(OBJECTS)
+
+.PHONY: clean all
--- dotter.c
+++ dotter.c
@@ -1,14 +1,14 @@
 #include "dotter.h"
 
 void mazes_dot(struct mazes_maze *maze, unsigned int *colors, FILE *stream) {
-  fprintf(stream, "/** MAZE: Rectangle %d %d **/\n", maze->column_count, maze->row_count);
+  fprintf(stream, "/** MAZE: Rectangle %zd %zd **/\n", maze->column_count, maze->row_count);
   fprintf(stream, "digraph Maze {\n");
   for (struct mazes_cell *cell = mazes_first_cell(maze); NULL != cell; cell = cell->next) {
-    fprintf(stream, "    node_%d_%d [shape=box,label=\"%d,%d\",_x=%d,_y=%d];\n",
+    fprintf(stream, "    node_%zd_%zd [shape=box,label=\"%zd,%zd\",_x=%zd,_y=%zd];\n",
 	    cell->column, cell->row, cell->column, cell->row, cell->column, cell->row);
     for (size_t i = 0; i < cell->links_length; i++) {
       struct mazes_cell *other = cell->links[i];
-      fprintf(stream, "    node_%d_%d -> node_%d_%d;\n",
+      fprintf(stream, "    node_%zd_%zd -> node_%zd_%zd;\n",
 	      cell->column, cell->row, other->column, other->row);
     }
   }
