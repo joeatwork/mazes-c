37f6bde984ec4c8d9f242203088335ddd1273b8d
Joe Bowers <joe.bowers@coreos.com>
2015-11-11 15:39:31 -0800

    serve a random maze

--- serve.sh
+++ serve.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 while true
 do
-    (cat <<EOF && $DIR/mazes --width=32 --height=32 --format=png) | nc -l 20202 > /dev/null
+    (cat <<EOF && $DIR/mazes --width=32 --height=32 --format=png --seed=$RANDOM) | nc -l 20202 > /dev/null
 HTTP/1.1 200 OK
 Content-Type: image/png
 Connection: close
