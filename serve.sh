fae2867cf066cc5e2cd3ba0cf78a2b558fc50fe1
Joe Bowers <joe.bowers@coreos.com>
2015-11-16 16:13:00 -0800

    read maze size from the environment

--- serve.sh
+++ serve.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SIZE=${SIZE:-32}
 
 while true
 do
-    (cat <<EOF && $DIR/mazes --width=32 --height=32 --format=png --seed=$RANDOM) | nc -l 20202 > /dev/null
+    (cat <<EOF && $DIR/mazes --width=$SIZE --height=$SIZE --format=png --seed=$RANDOM) | nc -l 20202 > /dev/null
 HTTP/1.1 200 OK
 Content-Type: image/png
 Connection: close
