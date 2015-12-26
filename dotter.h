f8e6a3aa1a2442cd0669c4c41f5871fad8a79f1c
Joe <joerbowers@gmail.com>
2015-12-25 21:05:28 -0800

    clear before merging the brand new way

--- dotter.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DOTTER_H__
-#define __DOTTER_H__
-
-#include <stdio.h>
-#include "mazes.h"
-
-void mazes_dot(struct mazes_maze *maze, unsigned int *colors, FILE *stream);
-
-#endif
--- utils.c
+++
@@ -1,5 +0,0 @@
-#include "utils.h"
-
-extern void *checked_calloc(size_t nmemb, size_t size);
-extern void *checked_malloc(size_t size);
-extern void *checked_realloc(void *ptr, size_t size);
